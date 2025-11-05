960dcdc1a9a645995c2563de584925eb5e19d619
kogarRin <kogarning@qq.com>
2025-11-05 20:01:40 +0800

    Reapply "fix：修复首页标签显示问题"
    This reverts commit 49441fab86735c1a5854c1bdbb25dff361412803.

--- src/js/edit/editHandle.js
+++ src/js/edit/editHandle.js
@@ -1,8 +1,9 @@
 import {ref, toRaw} from "vue";
-import {getNotesData, notesFromDb, updateNote} from "@/src/js/home/homeHandle.js";
+import {notesFromDb, updateNote} from "@/src/js/home/homeHandle.js";
 import {onBeforeRouteLeave} from "vue-router";
 import {ElMessage, ElMessageBox} from "element-plus";
 import {ElMessageConfig} from "@/src/js/config/messageType.js";
+import {searchResult} from "@/src/js/common/use/tool.js";
 
 export function useEditNote(route){
     //edit对象用于接收数据，作为渲染初始值
@@ -51,7 +52,9 @@ export function useEditNote(route){
             tagsRef.value.push(tag);
             inputValue.value = '';
             inputVisible.value = !inputVisible.value;
-            notesFromDb.value = await getNotesData();
+            const index = notesFromDb.value.findIndex(item => item.id === noteId);
+            notesFromDb.value[index].tags = tagsRef.value;
+            searchResult.value = [...notesFromDb.value];
             ElMessage(ElMessageConfig.buildConfig('success', '标签已保存', false, 1000));
             return null;
         } catch (error) {
@@ -75,8 +78,8 @@ export function useEditNote(route){
     }
 
     //删除标签
-    function deleteTags(noteId, tag){
-        window.electronAPI.deleteTagsInNotes(noteId, tag)
+    function deleteEditTags(noteId, tag){
+        window.electronAPI.deleteTags(noteId, tag)
         tagsRef.value.splice(tagsRef.value.indexOf(tag),1);
         ElMessage(ElMessageConfig.buildConfig('success', '已删除', false, 1000))
     }
@@ -120,6 +123,6 @@ export function useEditNote(route){
         cancelSetTag,
         saveClick,
         setRouteGuard,
-        deleteTags
+        deleteEditTags
     }
 }
