92dcc20f0620b3a93920ce16abc36ba875e2123b
kogarRin <kogarning@qq.com>
2025-11-14 00:01:59 +0800

    fix：修复段落计算逻辑：修复段落换行错误

--- src/components/editAndShow/showForm.vue
+++ src/components/editAndShow/showForm.vue
@@ -15,76 +15,55 @@ function backHome(){
     router.push({name: "home"});
 }
 
-// 格式化笔记内容
+// 优化笔记展示
 function textFormat(text){
+  text = text.replace(/\\+$/, '');
   try {
     if (!text || text.trim() === "") return [];
-    if (!/\n$/.test(text)) {
-      console.log("text:", text)
-     return text;
-    }
     console.log("text:", text)
     return text.split("\n").filter((item) => item.trim()!== "");
   } catch (error) {
-    console.log("return:", text)
     console.log(error);
     return [];
   }
 }
-function getPraArray(toFormatContent){
-  try {
-    if (!toFormatContent) return [];
-    if (Array.isArray(toFormatContent)) {
-      return toFormatContent;
-    }
-    String(toFormatContent).trim();
-    console.log(Array(1).fill(toFormatContent));
-    return Array(1).fill(toFormatContent);
-  } catch (error) {
-    console.log(error);
-    return [];
-  }
-}
+const praList = textFormat(toShowNote.value.content);
 //编辑页面
 function toEdit(){
   router.push({
     name: "edit",
-    params: {
-      id: toShowNote.value.id
-    }
   });
 }
 </script>
 
 <template>
 <div class="mainContainer">
-  <div class="spaceContainer">
-    <el-space
-        style="width: 100%;"
-        direction="vertical"
-        fill>
-      <el-card>
-        <div class="headerForm">
-          <h1 class="title">{{toShowNote}}</h1>
-          <div class="buttonsContainer">
-            <el-button text @click="backHome">
-              <el-icon size="18px"><Back /></el-icon>
-            </el-button>
-            <el-button type="primary" @click="toEdit">
-              <span>编辑</span>
-            </el-button>
-          </div>
+  <el-space
+      style="width: 100%;"
+      direction="vertical"
+      fill>
+    <el-card>
+      <div class="headerForm">
+        <h1 class="title">{{toShowNote.title}}</h1>
+        <div class="buttonsContainer">
+          <el-button text @click="backHome">
+            <el-icon size="18px"><Back /></el-icon>
+            <span>返回首页</span>
+          </el-button>
+          <el-button type="primary" @click="toEdit">
+            <span>继续编辑</span>
+          </el-button>
         </div>
-        <el-divider>
-        </el-divider>
-        <div class="contentForm">
-          <el-scrollbar>
-            <p v-for="(item, index) in awa" style="background-color: #808890" :key="index">{{item}}</p>
-          </el-scrollbar>
-        </div>
-      </el-card>
-    </el-space>
-  </div>
+      </div>
+      <el-divider>
+      </el-divider>
+      <div class="contentForm">
+        <el-scrollbar>
+          <p v-for="(item, index) in praList" :key="index">{{item}}</p>
+        </el-scrollbar>
+      </div>
+    </el-card>
+  </el-space>
 </div>
 </template>
 
