f18a8691f0aa590547ba6090767755440a6912d5
kogarRin <kogarning@qq.com>
2025-11-16 23:30:30 +0800

    fix：修改端口号

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,14 @@ import vue from '@vitejs/plugin-vue'
 import path from "path";
 import {fileURLToPath} from "url";
 
+//ESM的__dirname写法
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const viteConfig = defineConfig({
   plugins: [vue()],
   server: {
-    port: 5171,
+    port: 5000,
     strictPort: false
   },
   base: './',
